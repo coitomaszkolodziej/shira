d47eba78a37fb4126527db3c8237ab8a43d60997
unknown <grosio13@gmail.com>
2015-07-20 10:07:30 +0200

    maven

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.SecurityUtils;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
