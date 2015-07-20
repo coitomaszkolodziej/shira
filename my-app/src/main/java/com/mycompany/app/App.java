21cd8195b41191b185e94fec50d3ad158247a9ca
unknown <grosio13@gmail.com>
2015-07-20 13:10:21 +0200

    com

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,21 @@
 package com.mycompany.app;
 
 
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp.Factory;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Auth auth = Auth.getInstance();
+       
+        auth.login("radek", "radek");
+
+           
+
+
     }
 }
