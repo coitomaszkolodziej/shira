66040703d5d0bb1c3b1d620edec22b0c7afff810
Radek <grosio13@gmail.com>
2015-07-22 06:32:07 +0200

    2

---
+++ my-app2/src/main/java/com/mycompany/app/MyShiroModule.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.app;
+
+import com.google.inject.Provides;
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.guice.ShiroModule;
+import org.apache.shiro.realm.text.IniRealm;
+
+/**
+ *
+ * @author radek
+ */
+public class MyShiroModule extends ShiroModule {
+
+    @Override
+    protected void configureShiro() {
+        try {
+            bindRealm().toConstructor(IniRealm.class.getConstructor(Ini.class));
+        } catch (NoSuchMethodException e) {
+            addError(e);
+        }
+    }
+
+    @Provides
+    Ini loadShiroIni() {
+        return Ini.fromResourcePath("/home/radek/Pulpit/shira/my-app/shiro.ini");
+    }
+
+}
