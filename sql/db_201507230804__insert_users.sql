551becb035d0d5981871b9ba83b20265227b0ca8
unknown <grosio13@gmail.com>
2015-07-23 08:18:24 +0200

    inserrtt

---
+++ sql/db_201507230804__insert_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES( 1, 'radek', 'radek');
+INSERT INTO users VALUES( 2, 'marek', 'marek'); 
+INSERT INTO users VALUES( 3, 'lukasz', 'lukasz');
+INSERT INTO users VALUES( 4, 'malo_wazny', 'malo_wazny');
---
+++ sql/db_201507230806__insert_permissions.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO permissions VALUES(1, 'dodawanie', 1);
+INSERT INTO permissions VALUES(2, 'odejmowanie', 1);
+INSERT INTO permissions VALUES(3, 'mnozenie', 1);
+INSERT INTO permissions VALUES(4, 'dodawanie', 1);
+
+INSERT INTO permissions VALUES(5, 'dodawanie', 2);
+INSERT INTO permissions VALUES(6, 'mnozenie', 3);
+INSERT INTO permissions VALUES(7, 'stowka', 4);
+INSERT INTO permissions VALUES(8, 'dodawanie_2_3', 5);
+
+
