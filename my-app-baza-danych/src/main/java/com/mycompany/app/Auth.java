2eb5cebfe43947fcaa41f8764d251cbd3f3c795e
= <markon012@gmail.com>
2015-07-23 08:19:30 +0200

    auth.java

--- my-app-baza-danych/src/main/java/com/mycompany/app/Auth.java
+++ my-app-baza-danych/src/main/java/com/mycompany/app/Auth.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import static java.lang.Math.log;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
@@ -42,18 +43,14 @@ public class Auth {
     //private SecurityManager securityManager;
     private static transient Logger log;
     private SecurityManager securityManager;
-
-            
+    private Session session;        
     public void logout(){
         Subject currentUser = SecurityUtils.getSubject();
         currentUser.logout();
-
     }
-    public void login(String login, String pass) {
+    public boolean login(String login, String pass) {
         Subject currentUser = SecurityUtils.getSubject();
-
         Session session = currentUser.getSession();
-
         if (!currentUser.isAuthenticated()) {
             UsernamePasswordToken token = new UsernamePasswordToken(login, pass);
             token.setRememberMe(true);
@@ -61,18 +58,34 @@ public class Auth {
                 currentUser.login(token);
             } catch (UnknownAccountException uae) {
                 log.info("There is no user with username of " + token.getPrincipal());
+                return false;
             } catch (IncorrectCredentialsException ice) {
                 log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+                return false;
             } catch (LockedAccountException lae) {
                 log.info("The account for username " + token.getPrincipal() + " is locked.  "
                         + "Please contact your administrator to unlock it.");
+                return false;
             } catch (AuthenticationException ae) {
-
+                log.info("AuthenticationException was thrown");
+                return false;
             }
             log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
-
+            
         }
-
+        return true;
     }
-
+    
+    boolean myCheckRole(String roleIdentifier)  throws AuthorizationException {
+        try{
+            Subject currentUser = SecurityUtils.getSubject();
+            
+        }catch (AuthorizationException ae){
+            Subject currentUser = SecurityUtils.getSubject();
+            log.info ("User [" + currentUser.getPrincipal() + "] has no role " + roleIdentifier);
+            return false;
+        }
+        return true;
+    }
+    
 }
