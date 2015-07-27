2be4aa9e3fb57f8dc24252885d5e5623b883f981
unknown <grosio13@gmail.com>
2015-07-27 11:15:44 +0200

    TESTPOW

---
+++ com-byteslounge-jsf-war/src/main/java/com/byteslounge/jsf/war/UserLoginView.java
@@ -0,0 +1,60 @@
+package org.primefaces.showcase.view.overlay;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+import org.primefaces.context.RequestContext;
+ 
+@ManagedBean
+public class UserLoginView {
+     
+    private String username;
+     
+    private String password;
+	
+	@PostContruct
+	public void checkIsLogged(){
+		Subject currUser = SecurityUtils.getSubject();
+		if( currUser.isAutenticated()){
+			RequestContext context = RequestContext.getCurrentInstance();
+			FacesMessage = null;
+			message = new FacesMessage(.SEVERITY_INFO, "You are logged in.", "KTOS");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    public void login(ActionEvent event) {
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message = null;
+        boolean loggedIn = false;
+         
+        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
+            loggedIn = true;
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+        } else {
+            loggedIn = false;
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+        }
+         
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        context.addCallbackParam("loggedIn", loggedIn);
+    }   
+}
